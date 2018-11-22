42b37a25b8f448d5026ab0a7903826e8376359b6
cbc106013 <34334167+cbc106013@users.noreply.github.com>
2018-11-22 20:15:36 +0800

    Update captcha_model.py

--- captcha_model.py
+++ captcha_model.py
@@ -4,27 +4,29 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from PIL import Image
 import numpy as np
 import csv
-import cv2
+
+
+
 
 def rgb2gray(rgb):
     r, g, b = rgb[:,:,:,0], rgb[:,:,:,1], rgb[:,:,:,2]
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray
 
-# letters = "0123456789abcdefghijklimnpqrstuvwxyzABCDEFGHIJKLIMNPQRSTUVWXYZ"
-letters = "0123456789"
+letters = "0123456789abcdefghijklimnpqrstuvwxyzABCDEFGHIJKLIMNPQRSTUVWXYZ"
+# letters = "0123456789"
 def toonehot(text):
     labellist = []
     for letter in text:
-        onehot = [0 for _ in range(10)]
+        onehot = [0 for _ in range(63)]
         num = letters.find(letter)
         onehot[num] = 1
         labellist.append(onehot)
     return labellist
 
 #==========================train_data
-traincsv = open('./number_train.csv', 'r', encoding = 'utf8')
-train_data = np.stack([np.array(Image.open("D:/cbc106013/captcha_cnn/captcha/number_train/" + row[0] + ".jpg"))/255.0 for row in csv.reader(traincsv)])
+traincsv = open('./train_label.csv', 'r', encoding = 'utf8')
+train_data = np.stack([np.array(Image.open("/home/cbc106013/deep_learning/captcha/train_captcha/" + row[0] + ".jpg"))/255.0 for row in csv.reader(traincsv)])
 
 #==========================rgb2gray
 train_data=rgb2gray(train_data)
@@ -42,7 +44,7 @@ print(train_data.shape)
 digit = []
 
 for i in range(1,5,1):
-    file = open('number_train.csv', 'r')  # csv_file_name
+    file = open('train_label.csv', 'r')  # csv_file_name
     reader = csv.reader(file)
     for row in reader:
         digit.append(row[i])
@@ -51,7 +53,7 @@ print(digit)
 #==========================csv2one_hot
 digit = np.array(toonehot(digit))
 #==========================2y
-train_label = digit.reshape(4,-1,10)
+train_label = digit.reshape(4,-1,63)
 print(train_label)
 print(train_label.shape)
 
@@ -59,8 +61,8 @@ print(train_label.shape)
 train_label = [arr for arr in np.asarray(train_label)]
 print(train_label)
 #==========================test_data
-testcsv = open('./number_test.csv', 'r', encoding = 'utf8')
-test_data = np.stack([np.array(Image.open("D:/cbc106013/captcha_cnn/captcha/number_test/" + row[0] + ".jpg"))/255.0 for row in csv.reader(testcsv)])
+testcsv = open('./test_label.csv', 'r', encoding = 'utf8')
+test_data = np.stack([np.array(Image.open("/home/cbc106013/deep_learning/captcha/test_captcha/" + row[0] + ".jpg"))/255.0 for row in csv.reader(testcsv)])
 
 #==========================rgb2gray
 test_data=rgb2gray(test_data)
@@ -78,16 +80,16 @@ test_data= test_data.reshape(-1,60,160,1)
 digit = []
 
 for i in range(1,5,1):
-    file = open('number_test.csv', 'r')  # csv_file_name
+    file = open('test_label.csv', 'r')  # csv_file_name
     reader = csv.reader(file)
     for row in reader:
         digit.append(row[i])
 
-# print(digit)
+print(digit)
 #==========================csv2one_hot
 digit = np.array(toonehot(digit))
 #==========================2y
-test_label = digit.reshape(4,-1,10)
+test_label = digit.reshape(4,-1,63)
 print(test_label)
 print(test_label.shape)
 test_label = [arr for arr in np.asarray(test_label)]
@@ -97,11 +99,19 @@ print("Creating CNN model...")
 # in = Input((60,160))
 input = Input(shape=(60,160,1), name='Input')
 out = input
+out = Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu')(out)
+out = Conv2D(filters=8, kernel_size=(3, 3), activation='relu')(out)
+out = BatchNormalization()(out)
+out = Conv2D(filters=16, kernel_size=(3, 3), padding='same', activation='relu')(out)
+out = Conv2D(filters=16, kernel_size=(3, 3), activation='relu')(out)
+out = BatchNormalization()(out)
+# out = MaxPooling2D(pool_size=(2, 2))(out)
 out = Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu')(out)
 out = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(out)
 out = BatchNormalization()(out)
 out = MaxPooling2D(pool_size=(2, 2))(out)
 out = Dropout(0.3)(out)
+
 out = Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(out)
 out = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(out)
 out = BatchNormalization()(out)
@@ -113,14 +123,13 @@ out = BatchNormalization()(out)
 out = MaxPooling2D(pool_size=(2, 2))(out)
 out = Dropout(0.3)(out)
 out = Conv2D(filters=256, kernel_size=(3, 3), activation='relu')(out)
-out = BatchNormalization()(out)
 out = MaxPooling2D(pool_size=(2, 2))(out)
 out = Dropout(0.3)(out)
 out = Flatten()(out)
-out = [Dense(10, name='digit1', activation='softmax')(out),\
-    Dense(10, name='digit2', activation='softmax')(out),\
-    Dense(10, name='digit3', activation='softmax')(out),\
-    Dense(10, name='digit4', activation='softmax')(out)]
+out = [Dense(63, name='digit1', activation='softmax')(out),\
+    Dense(63, name='digit2', activation='softmax')(out),\
+    Dense(63, name='digit3', activation='softmax')(out),\
+    Dense(63, name='digit4', activation='softmax')(out)]
 model = Model(input, out)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()#show
@@ -129,11 +138,11 @@ model.summary()#show
 filepath="./model/captcha4.h5"
 #==========================每次epoch完會檢查一次，如果比先前最佳的acc高，就會儲存model到filepath
 checkpoint = ModelCheckpoint(filepath, monitor='val_digit3_acc', verbose=1, save_best_only=True, mode='max')
-#===========================也就是在驗證集的val_digit4_acc連續5次不再下降時，就會提早結束訓練
-earlystop = EarlyStopping(monitor='val_digit4_acc', patience=20, verbose=1, mode='auto')
+#===========================也就是在驗證集的val_digit4_acc 下降後60次提前停止訓練
+#earlystop = EarlyStopping(monitor='val_digit3_acc', patience=60, verbose=1, mode='auto')
 #===========================圖形化界面
 tensorBoard = TensorBoard(log_dir = "./logs", histogram_freq = 1)
 #===========================回調函數
-callbacks_list = [checkpoint, earlystop, tensorBoard]
+callbacks_list = [checkpoint, tensorBoard] #earlystop
 #===========================
-model.fit(train_data, train_label, batch_size=400, epochs=100, verbose=1, validation_data=(test_data, test_label), callbacks=callbacks_list)
+model.fit(train_data, train_label, batch_size=400, epochs=150, verbose=1, validation_data=(test_data, test_label), callbacks=callbacks_list)
